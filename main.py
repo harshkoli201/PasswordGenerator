528e7898726e5cbc348e87a6516b06b8d43531a2
Harsh Koli <harshkoli201@gmail.com>
2022-02-01 21:16:14 +0530

    Done

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-
+from tkinter import *
 import random
 import string
-import tkinter
 
-app = tkinter.Tk()
+app = Tk()
 app.title("Password Generator")
 app.geometry('500x500')
 app.configure(bg='grey')
 
-
-Lab = tkinter.Label(app,text = "Welcome to Password Generator",bg= "gray",font='Helvetica 10 bold').place(x = 150,y = 20)  
-
-Lab1 = tkinter.Label(app,text = "Enter the length of your password",bg= "gray",font='Helvetica 10 bold').place(x = 150,y = 60)  
-
-a=tkinter.Entry(app, width=30).place(x = 165,y = 100)  
-
-
 def cal():
-    l = tkinter.int(a.get())
-    tot = string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
-    f = random.sample(tot,l)
-    password = "".join(f)
+        l = int(b.get())
+        tot = string.ascii_lowercase+string.ascii_uppercase+string.digits
+        f = random.sample(tot,l)
+        p = "".join(f)
+        a.config(text="Your Password is = %s" % p)  
 
-b = tkinter.Button(app, text="Generate Password", command=cal).place(x = 195,y = 150)  
+Lab = Label(app,text = "Welcome to Password Generator",bg= "gray",font='Helvetica 10 bold').place(x = 150,y = 20)  
+
+Lab1 = Label(app,text = "Enter the length of your password",bg= "gray",font='Helvetica 10 bold').place(x = 150,y = 60)  
+
+b = Entry(app, width=230)
+b.pack(padx=150, pady= 100)  
+
+
+button = Button(app, text="Generate Password", command=cal).place(x = 195,y = 150)  
+
+
+a = Label(app, text='',bg= "gray")
+a.pack()
 
 app.mainloop()
